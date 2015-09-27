d5d61585426c43f7bd6814b68f59e6bb59820cdd
hamsham <mileslacey@gmail.com>
2015-09-26 17:41:02 -0700

    Migrated LightScript into its own repo.

---
+++ src/script/setup.cpp
@@ -0,0 +1,7 @@
+
+#include "lightsky/script/setup.h"
+#include "lightsky/script/scriptVariable.h"
+#include "lightsky/script/scriptFunctor.h"
+
+template class ls::utils::pointer<ls::script::variable>;
+template class ls::utils::pointer<ls::script::functor>;
